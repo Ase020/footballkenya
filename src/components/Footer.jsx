861536aee181e9a2a1b5347d1f4f561dce19b9a0
felixNyalenda <felixasejunior@gmail.com>
2024-03-02 15:09:42 +0300

    footer build

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,105 @@
+import { Link } from "react-router-dom";
+import { logo } from "../assets";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <div className="bg-primary">
+      <footer className="max-width flex-center flex-col px-2 sm:px-4 md:px-20 py-4 md:py-12 space-y-4 md:space-y-8 text-white">
+        <div className="w-full flex flex-col md:flex-row items-start justify-between gap-8">
+          <div className="w-full md:w-1/4 flex-center flex-col gap-3">
+            <img src={logo} alt="logo" className="w-20 object-contain" />
+
+            <h3 className="uppercase text-base font-semibold">
+              FOOTBALL KENYA FEDERATION
+            </h3>
+          </div>
+
+          <div className="w-full md:w-3/4 flex justify-between">
+            <div className="w-full flex flex-col gap-4">
+              <h4 className="text-[#D0D5DD] text-sm font-semibold">Company</h4>
+
+              <nav className="flex flex-col gap-3 text-base font-medium">
+                <Link to="/" className="">
+                  About Us
+                </Link>
+                <Link to="/" className="">
+                  Contact Us
+                </Link>
+                <Link to="/" className="">
+                  FAQs
+                </Link>
+              </nav>
+            </div>
+
+            <div className="w-full flex flex-col gap-4">
+              <h4 className="text-[#D0D5DD] text-sm font-semibold">
+                Resources
+              </h4>
+
+              <nav className="flex flex-col gap-3 text-base font-medium">
+                <Link to="/" className="">
+                  News
+                </Link>
+                <Link to="/" className="">
+                  Help Centre
+                </Link>
+                <Link to="/" className="">
+                  Support
+                </Link>
+              </nav>
+            </div>
+
+            <div className="w-full flex flex-col gap-4">
+              <h4 className="text-[#D0D5DD] text-sm font-semibold">Leagues</h4>
+
+              <nav className="flex flex-col gap-3 text-base font-medium">
+                <Link to="/" className="">
+                  FKF PL
+                </Link>
+                <Link to="/" className="">
+                  FKF NSL
+                </Link>
+                <Link to="/" className="">
+                  FKF WNSL
+                </Link>
+                <Link to="/" className="">
+                  Mozzart Bet Cup
+                </Link>
+                <Link to="/" className="">
+                  Women Cup
+                </Link>
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        <hr className="w-full h-1" />
+
+        <div className="flex-col md:flex-row gap-4 w-full flex-between text-base">
+          <div className="w-full md:w-3/5 flex flex-col md:flex-row items-center gap-2 md:gap-4 capitalize">
+            <p className="">© {year}</p>
+            <p className="">football kenya federation</p>
+            <p className="">All rights reserved</p>
+          </div>
+
+          <div className="w-full md:w-2/5 flex items-center justify-between">
+            <Link to="/" className="">
+              Terms
+            </Link>
+            <Link to="/" className="">
+              Privacy
+            </Link>
+            <Link to="/" className="">
+              Cookies
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+
 import { logo } from "../assets";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="bg-transparent px-10 max-width flex-between border h-20">
+    <header className="relative bg-transparent px-2 md:px-10 max-width flex-between h-20">
       <Link to="/">
         <img src={logo} alt="logo" className="object-contain w-16" />
       </Link>
 
-      <nav className="flex-between space-x-8 text-base font-semibold">
+      <nav className="hidden md:flex-between space-x-8 text-base font-semibold">
         <Link to="/" className="underline underline-offset-4">
           Home
         </Link>
@@ -19,6 +25,55 @@ const Navbar = () => {
           Rules and governance
         </Link>
         <Link to="/" className="underline-offset-4">
+          Integrity
+        </Link>
+      </nav>
+
+      <div className="flex-center md:hidden mr-3 px-1">
+        {isOpen ? (
+          <CloseRoundedIcon
+            fontSize="large"
+            onClick={() => setIsOpen((prev) => !prev)}
+          />
+        ) : (
+          <MenuRoundedIcon
+            fontSize="large"
+            onClick={() => setIsOpen((prev) => !prev)}
+          />
+        )}
+      </div>
+
+      {/* mobile nav */}
+      <nav
+        className="absolute top-20 left-0 bg-white h-[calc(100vh-80px)] w-screen flex-center text-center px-2 flex-col gap-12 text-3xl"
+        style={isOpen ? { display: "flex" } : { display: "none" }}
+      >
+        <Link
+          to="/"
+          className="underline underline-offset-4"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          Home
+        </Link>
+        <Link
+          to="/"
+          className="underline-offset-4"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          Leagues & Tournaments
+        </Link>
+        <Link
+          to="/"
+          className="underline-offset-4"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          Rules and governance
+        </Link>
+        <Link
+          to="/"
+          className="underline-offset-4"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
           Integrity
         </Link>
       </nav>
